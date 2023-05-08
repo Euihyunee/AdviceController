4a753973be25c23aa760f119ca7aeb68883b1ecb
[Mac] euihyunee <euihyunee@github.com>
2023-05-08 19:50:35 +0900

    [ADD] Create Project

---
+++ adviceControllerTest/src/main/java/com/example/adviceControllerTest/AdviceControllerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.adviceControllerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceControllerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdviceControllerTestApplication.class, args);
+	}
+
+}
---
+++ adviceControllerTest/src/test/java/com/example/adviceControllerTest/AdviceControllerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adviceControllerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviceControllerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
