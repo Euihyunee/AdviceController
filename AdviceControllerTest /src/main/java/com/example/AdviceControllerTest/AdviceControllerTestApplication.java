ee25094b2c5ddf1b5df70006334fd738981a4ef2
[Mac] euihyunee <euihyunee@github.com>
2023-05-09 17:25:54 +0900

    [ADD] 프로젝트 추가

---
+++ AdviceControllerTest /src/main/java/com/example/AdviceControllerTest/AdviceControllerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.AdviceControllerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceControllerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdviceControllerTestApplication.class, args);
+	}
+
+}
